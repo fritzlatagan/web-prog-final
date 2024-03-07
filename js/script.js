9a1da93efa481fe4131c03926ab9d2eb712b6b05
fritzlatagan <fgLatagan@mcm.edu.ph>
2024-03-07 10:37:11 +0800

    initial push
    added files and images for final project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** 
+ *  PRELOAD
+ *  loadign will end after document is loaded
+ */
+
+const preloader = document.querySelector("[data-preload]");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
